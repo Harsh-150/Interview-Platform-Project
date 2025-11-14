e6cd1c6ae629b192108ebba602d0677ae42fa71b
Harsh Gupta <harsh.10gh@gmail.com>
2025-11-14 15:55:48 +0530

    one error corrected

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -7,14 +7,14 @@ export const inngest = new Inngest({id: "interview-platform"});
 const syncUser = inngest.createFunction(
     {id: 'sync-user'},
     {event: 'clerk/user.created'},
-    async({event}) =>{
+    async({event}) => {
         await connectDB()
 
         const {id, email_addresses, first_name, last_name, image_url} = event.data;
 
         const newUser = {
             clerkId: id,
-            email: email_addresses[0].email_addresses[0].email_address,
+            email: email_addresses[0]?.email_address,
             name: `${first_name || ""}  ${last_name || ""}`,
             profileImage: image_url || ""
         }
