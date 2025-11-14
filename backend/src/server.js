a0c21e1a4e8399f50cc8a25b616940dd6cfcec7f
Harsh Gupta <harsh.10gh@gmail.com>
2025-11-14 14:22:05 +0530

    more changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,6 +31,12 @@ app.get('/contact', (req,res)=>{
     })
 })
 
+app.get('/{*any}', (req,res)=>{
+    res.status(200).json({
+        'description': "This is the default page."
+    })
+})
+
 // make ready for deployment
 if(ENV.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/dist')))
