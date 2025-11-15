7c85e6479156c17b1703850611e093860a583fec
Harsh Gupta <harsh.10gh@gmail.com>
2025-11-15 17:55:38 +0530

    session routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import {inngest, functions} from './lib/inngest.js'
 import {clerkMiddleware} from '@clerk/express'
 import { protectRoute } from './middleware/protectRoute.js'
 import chatRoutes from './routes/chatRoutes.js'
+import sessionRoutes from './routes/sessionRoutes.js'
 
 const app = express()
 
@@ -22,6 +23,7 @@ app.use(clerkMiddleware()) // this adds auth field to request object: req.auth()
 // --- API ROUTES ---
 app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/chat',chatRoutes)
+app.use('/api/sessions', sessionRoutes)
 
 app.get('/api/about', (req,res)=>{  
     res.status(200).json({
